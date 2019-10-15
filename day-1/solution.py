c7de4e803b9a71031b4a9a492c1778c80bc517c5
Damian Ziemba <nazriel@dzfl.pl>
2019-10-15 23:25:07 +0200

    Add test-cases for Day-1 problem

--- day-1/solution.py
+++ day-1/solution.py
@@ -1,6 +1,38 @@
+import unittest
+
 def does_adds_up(numbers_list: list, sum: int) -> bool:
     """
     Check whenever any two numbers provided in numbers_list adds up to sum.
     """
     return False
 
+
+class TestDoesAddsUp(unittest.TestCase):
+    def test_positive(self):
+        self.assertTrue(does_adds_up([10, 15, 3, 7], 17))
+        self.assertTrue(does_adds_up([3, 4, 9, 12], 7))
+        self.assertTrue(does_adds_up([4, 4], 8))
+        self.assertTrue(does_adds_up([1, 2, 5], 3))
+
+    def test_negative(self):
+        self.assertFalse(does_adds_up([10, 15, 3, 7], 5))
+        self.assertFalse(does_adds_up([3, 4, 9, 12], 4))
+        self.assertFalse(does_adds_up([4, 4], 3))
+        self.assertFalse(does_adds_up([1, 2, 5], 1))
+
+    def test_corner_cases(self):
+        self.assertFalse(does_adds_up([], 5))
+        self.assertFalse(does_adds_up([1, 2], 0))
+        self.assertFalse(does_adds_up([99, -1], -1))
+        self.assertTrue(does_adds_up([-2, -1], -3))
+
+    def test_wrong_args(self):
+        with self.assertRaises(TypeError):
+            does_adds_up([-2, -1], "ff")
+            does_adds_up(1, 9)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
