478e1b31c5244652c4679a1d1c870cb25bef3923
Damian Ziemba <nazriel@dzfl.pl>
2019-10-19 03:42:33 +0200

    Add test cases
    refs Day-2

--- day-2/solution.cpp
+++ day-2/solution.cpp
@@ -1,3 +1,5 @@
+#define BOOST_TEST_MODULE day2
+#include <boost/test/included/unit_test.hpp>
 #include <vector>
 #include <cstdint>
 
@@ -6,3 +8,42 @@ std::vector<std::uint32_t> solution(const std::vector<std::uint32_t>&)
     return {};
 }
 
+BOOST_AUTO_TEST_CASE( possitive )
+{
+    std::vector<std::uint32_t> lhs, rhs;
+
+    lhs = solution({1, 2, 3, 4, 5});
+    rhs = {120, 60, 40, 30, 24};
+    BOOST_CHECK_EQUAL(lhs, rhs);
+
+    lhs = solution({3, 2, 1});
+    rhs = {2, 3, 6};
+    BOOST_CHECK_EQUAL(lhs, rhs);
+}
+
+BOOST_AUTO_TEST_CASE_EXPECTED_FAILURES( negative, 2 )
+BOOST_AUTO_TEST_CASE( negative )
+{
+    std::vector<std::uint32_t> lhs, rhs;
+
+    lhs = solution({1, 2, 3, 4, 5});
+    rhs = {120, 60, 40, 30, 25};
+    BOOST_CHECK_EQUAL(lhs, rhs);
+
+    lhs = solution({3, 2, 1});
+    rhs = {2, 3, 7};
+    BOOST_CHECK_EQUAL(lhs, rhs);
+}
+
+BOOST_AUTO_TEST_CASE( corner )
+{
+    std::vector<std::uint32_t> lhs, rhs;
+
+    lhs = solution({});
+    rhs = {};
+    BOOST_CHECK_EQUAL(lhs, rhs);
+
+    lhs = solution({3});
+    rhs = {0};
+    BOOST_CHECK_EQUAL(lhs, rhs);
+}
