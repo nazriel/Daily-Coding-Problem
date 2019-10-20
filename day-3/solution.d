d83bfb691376b438a4563b991df8def8fd70f5db
Damian Ziemba <nazriel@dzfl.pl>
2019-10-20 19:12:25 +0200

    Unittests for serialize() implementation.
    refs Day-3

--- day-3/solution.d
+++ day-3/solution.d
@@ -18,6 +18,25 @@ string serialize(Node node)
     return "";
 }
 
+unittest
+{
+    auto node = new Node("root", new Node("root.left", new Node("root.left.left")), new Node("root.right"));
+    auto s = serialize(node);
+    assert(s == `n(v="root", l=n(v="root.left", l=n(v="root.left.left")), r=n(v="root.right"))`, s);
+
+    node = new Node("");
+    s = serialize(node);
+    assert(s == `n(v="")`, s);
+
+    node = new Node(`"`);
+    s = serialize(node);
+    assert(s == `n(v="\"")`, s);
+
+    node = new Node(``, new Node(""), new Node(""));
+    s = serialize(node);
+    assert(s == `n(v="", l=n(v=""), r=n(v=""))`, s);
+}
+
 Node unserialize(string str)
 {
     return new Node();
