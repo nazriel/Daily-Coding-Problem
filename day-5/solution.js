dcff7553fc6a2561c59fe2cdf0ef9898119d1b88
Damian Ziemba <nazriel@dzfl.pl>
2019-10-20 20:10:34 +0200

    stubs for car, cdr
    + cons implementation
    + makefile
    
    refs Day-5

---
+++ day-5/solution.js
@@ -0,0 +1,18 @@
+function cons(a, b)
+{
+    const pair = (f) => {
+        return f(a, b);
+    }
+    return pair
+}
+
+function car(pair)
+{
+    return null;
+}
+
+function cdr(pair)
+{
+    return null;
+}
+
