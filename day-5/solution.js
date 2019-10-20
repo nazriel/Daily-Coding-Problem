20456d434ea17c19aec6fb429aacbffe1c63de5f
Damian Ziemba <nazriel@dzfl.pl>
2019-10-20 20:11:15 +0200

    Tests
    refs Day-5

--- day-5/solution.js
+++ day-5/solution.js
@@ -16,3 +16,13 @@ function cdr(pair)
     return null;
 }
 
+let result = null;
+
+result = cons(3, 4)((a, b) => a + b);
+console.assert(result == 7, result);
+
+result = car(cons(3, 4));
+console.assert(result == 3, result);
+
+result = cdr(cons(3, 4));
+console.assert(result == 4, result);
