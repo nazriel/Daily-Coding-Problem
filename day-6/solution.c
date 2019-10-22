f50c275ec9806f5497c806072d49f9c4432ab802
Damian Ziemba <nazriel@dzfl.pl>
2019-10-22 08:20:22 +0200

    Routines for adding nodes, printing content of all nodes, helper for creating node, stub for get
    Still working as regular double linked list
    
    refs Day-6

--- day-6/solution.c
+++ day-6/solution.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <stdbool.h>
 
 typedef struct Node
@@ -19,11 +20,62 @@ typedef struct XorLinkedList
     size_t len;
 } XorLinkedList;
 
-void add(XorLinkedList *list, void *value, size_t size)
+Node* newNode(void* value, size_t size)
 {
+    Node* n = (Node*) malloc(sizeof(Node));
+    if (!n)
+    {
+        assert(false);
+    }
+
+    memset(n, 0, sizeof(Node));
+    n->value = value;
+    n->size = size;
+
+    return n;
+}
+void add(XorLinkedList* list, void *value, size_t size)
+{
+    Node* n = newNode(value, size);
+    if (list->head == NULL)
+{
+        list->head = n;
+        list->tail = n;
+        list->len = 1;
+        return;
+    }
+    list->tail->next = n;
+    n->prev = list->tail;
+
+    list->tail = n;
+    list->len++;
+}
+
+void* get(XorLinkedList* list, size_t index)
+{
+    (void) index;
     (void) list;
-    (void) value;
-    (void) size;
+    return (void *)0xdeadbeef;
+}
+
+void printAll(XorLinkedList* list)
+{
+    Node* curr = list->head;
+    Node* prev = NULL;
+
+    while (curr)
+    {
+        printf("%p (next: %p, prev: %p): %.*s\n", (void*) curr, (void*) curr->next, (void*) curr->prev, (int) curr->size, (char*) curr->value);
+        prev = curr;
+        curr = curr->next;
+    }
+
+    curr = prev;
+    while (curr)
+    {
+        printf("%p (next: %p, prev: %p): %.*s\n", (void*) curr, (void*) curr->next, (void*) curr->prev, (int) curr->size, (char*) curr->value);
+        curr = curr->prev;
+    }
 }
 
 int main()
