dbcef0d9dfea7f253e75ff5fe72023576048285b
Damian Ziemba <nazriel@dzfl.pl>
2019-10-23 11:54:50 +0200

    Add routine for "removing" all negative numbers (and 0).
    It puts negative numbers at the begging by swaping positions with other positive numbers.
    Then it slices original array so only part of array with positives numbers stays available.
    
    refs Day-4

--- day-4/solution.go
+++ day-4/solution.go
@@ -1,6 +1,20 @@
 package main
 
+func swipeNegatives(inputs []int) []int {
+	j := 0
+	// O(n)
+	for i, number := range inputs {
+		if number <= 0 {
+			inputs[j], inputs[i] = inputs[i], inputs[j]
+			j++
+		}
+	}
+
+	return inputs[j:]
+}
+
 func solution(inputs []int) int {
+	inputs = swipeNegatives(inputs) // input is sliced so Space complexity stays intact
 	return 0
 }
 
