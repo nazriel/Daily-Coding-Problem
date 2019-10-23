fd66a3612c7c1731433593322b35e2cad7e2e04e
Damian Ziemba <nazriel@dzfl.pl>
2019-10-23 12:01:25 +0200

    Since after swiping we have only positive numbers available it is possible now to use them for other purpose.
    We can use existing array of elements as kind of Set that will indicate whenever given number was available.
    Example: if we see number 3, we can make number in array under index 3 negative.
    
    Then specification of the task, which states "smallest, positive, not existing number" ensures that the needle we are looking for will be smaller or equal to the length of array itself.
    
    The +1 and -1 offsets are used to remove difference between 0-indexed arrays and the smallest positive number which is 1.
    
    refs Day-4

--- day-4/solution.go
+++ day-4/solution.go
@@ -1,5 +1,13 @@
 package main
 
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+
+	return n
+}
+
 func swipeNegatives(inputs []int) []int {
 	j := 0
 	// O(n)
@@ -13,9 +21,31 @@ func swipeNegatives(inputs []int) []int {
 	return inputs[j:]
 }
 
+func markVistited(inputs []int) {
+	// O(n)
+	for i, number := range inputs {
+		if abs(number)-1 < len(inputs) {
+			if inputs[abs(inputs[i])-1] > 0 {
+				inputs[abs(inputs[i])-1] = -inputs[abs(inputs[i])-1]
+			}
+		}
+	}
+}
+
+// T: O(3n) in total -> O(n)
+// S: O(1)
 func solution(inputs []int) int {
 	inputs = swipeNegatives(inputs) // input is sliced so Space complexity stays intact
-	return 0
+	markVistited(inputs)
+
+	// O(n)
+	for i, n := range inputs {
+		if n > 0 {
+			return i + 1
+		}
+	}
+
+	return len(inputs) + 1
 }
 
 func main() {
