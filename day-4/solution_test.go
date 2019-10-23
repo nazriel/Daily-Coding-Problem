96fad9f9a758c1b6c9d9c03b59b8b4c7c715ac41
Damian Ziemba <nazriel@dzfl.pl>
2019-10-23 11:51:46 +0200

    Add tests
    refs Day-4

--- day-4/solution_test.go
+++ day-4/solution_test.go
@@ -2,7 +2,29 @@ package main
 
 import (
 	"testing"
+
+	"gotest.tools/assert"
 )
 
 func TestRegular(t *testing.T) {
+	result := solution([]int{7, 2, 5, 0, 1})
+	assert.Assert(t, result == 3, result)
+
+	assert.Assert(t, solution([]int{7, 2, 5, 0, 1}) == 3)
+
+	assert.Assert(t, solution([]int{3, 4, -1, 1}) == 2)
+
+	assert.Assert(t, solution([]int{1, 2, 0}) == 3)
+
+	assert.Assert(t, solution([]int{1, 2, 4, 0}) == 3)
+
+	assert.Assert(t, solution([]int{1}) == 2)
+
+	assert.Assert(t, solution([]int{0}) == 1)
+
+	assert.Assert(t, solution([]int{-1}) == 1)
+
+	assert.Assert(t, solution([]int{3, 7, 9, 55, 57, 1, 2}) == 4)
+
+	assert.Assert(t, solution([]int{-3, -2}) == 1)
 }
